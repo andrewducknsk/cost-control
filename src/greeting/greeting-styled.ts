6fd37d476985862cac4dab0fba4253c79ddb8ac3
Andrew <andewkinev@yandex.ru>
2020-02-24 22:27:55 +0700

    fix greeting page

--- src/greeting/greeting-styled.ts
+++ src/greeting/greeting-styled.ts
@@ -5,9 +5,13 @@ const GreetingStyled = styled.section``;
 
 const GreetingTitleStyled = styled.h2``;
 
-const GreetingDescriptionStyled = styled.p``;
+const GreetingDescriptionStyled = styled.p`
+  margin-top: 30px;
+`;
 
 const GreetingButtonStyled = styled(Styled.Button)`
+  display: block;
+  margin: 50px auto 0;
   width: 400px;
   height: 50px;
 `;
